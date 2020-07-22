49c452c5ad62801324c7553b33b23dc1f7fc8c99
Blake <blake.mcelroy233@yahoo.com>
2020-07-21 22:44:07 -0400

    Deployment Assignment

---
+++ Software Development Assignments/src/Server.java
@@ -0,0 +1,77 @@
+import java.io.*;
+import java.net.*;
+import java.util.Date;
+import javafx.application.Application;
+import javafx.application.Platform;
+import javafx.scene.Scene;
+import javafx.scene.control.ScrollPane;
+import javafx.scene.control.TextArea;
+import javafx.stage.Stage;
+
+public class Server extends Application {
+  @Override // Override the start method in the Application class
+  public void start(Stage primaryStage) {
+    // Text area for displaying contents
+    TextArea ta = new TextArea();
+
+    // Create a scene and place it in the stage
+    Scene scene = new Scene(new ScrollPane(ta), 450, 200);
+    primaryStage.setTitle("Server"); // Set the stage title
+    primaryStage.setScene(scene); // Place the scene in the stage
+    primaryStage.show(); // Display the stage
+    
+    new Thread( () -> {
+      try {
+        // Create a server socket
+        ServerSocket serverSocket = new ServerSocket(8000);
+        Platform.runLater(() ->
+          ta.appendText("Server started at " + new Date() + '\n'));
+  
+        // Listen for a connection request
+        Socket socket = serverSocket.accept();
+  
+        // Create data input and output streams
+        DataInputStream inputFromClient = new DataInputStream(
+          socket.getInputStream());
+        DataOutputStream outputToClient = new DataOutputStream(
+          socket.getOutputStream());
+  
+        while (true) {
+          // Receive radius from the client
+          int number = inputFromClient.readInt();
+          boolean prime = true;
+  
+          // Compute if Prime
+          if (number <= 1) {
+        	  prime = false;
+          } else {
+        	  for (int i = 2; i < number; i++) {
+        		  if ((number % i) == 0) {
+        			  prime = false;
+        			  break;
+        		  }
+        	  }
+          }
+  
+          // Send area back to the client
+          outputToClient.writeBoolean(prime);
+  
+          Platform.runLater(() -> {
+            ta.appendText("Number Recieved from Client: " + number);
+          });
+        }
+      }
+      catch(IOException ex) {
+        ex.printStackTrace();
+      }
+    }).start();
+  }
+
+  /**
+   * The main method is only needed for the IDE with limited
+   * JavaFX support. Not needed for running from the command line.
+   */
+  public static void main(String[] args) {
+    launch(args);
+  }
+}
